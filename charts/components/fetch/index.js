8fe2939e03c2cd141f143a16c314658c435ac824
Bilal Ahmed <bilal91_@hotmail.com>
2021-03-10 23:10:32 +0000

    have response from trending search
    will add use effect and use conditional rendering to make this work tmrw

--- charts/components/fetch/index.js
+++ charts/components/fetch/index.js
@@ -1,4 +1,22 @@
-const Fetch = () => (<div>hello</div>);
+// use the coin geko api and make the request fot the7 7 most searched on coingeko
+import { useState } from 'react';
+
+const Fetch = () => {
+  const [coins, setCoins] = useState(null);
+  fetch('https://api.coingecko.com/api/v3/search/trending')
+    .then((res) => res.json())
+    .then((data) => {
+      setCoins(data.coins);
+    });
+
+  return (
+    <div>
+      {coins.map((item) => {
+        <h1>{item.name}</h1>;
+      })}
+    </div>
+  );
+};
 
 // eslint-disable-next-line linebreak-style
 export default Fetch;
