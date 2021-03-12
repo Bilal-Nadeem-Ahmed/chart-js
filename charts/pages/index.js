ffa0b28655174af233b77e7a7b5b5287ad47427a
Bilal Ahmed <bilal91_@hotmail.com>
2021-03-12 23:16:47 +0000

    working on the first chart

--- charts/pages/index.js
+++ charts/pages/index.js
@@ -1,34 +1,11 @@
 import Head from 'next/head';
-import { Bar } from 'react-chartjs-2';
+
 import styles from '../styles/Home.module.css';
 
 import Trending from '../components/trending';
 
 export default function Home() {
-  const data = {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)',
-      ],
-      borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)',
-      ],
-      borderWidth: 1,
-    }],
-  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -37,14 +14,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <Bar
-          data={data}
-          width={400}
-          height={200}
-          options={{
-            maintainAspectRatio: true,
-          }}
-        />
+        
         <Trending />
       </main>
 
