5eaa55520e2761187202476dc1e76f32ebb43105
Bilal Ahmed <bilal91_@hotmail.com>
2021-03-13 00:19:22 +0000

    charts x and y axis showing the correct info, but cannot see line

--- charts/components/trendingChart/index.js
+++ charts/components/trendingChart/index.js
@@ -2,35 +2,71 @@ import { useEffect, useState } from 'react';
 import { Line } from 'react-chartjs-2'
 
 const TrendingChart = ({ idToFetch }) => {
-  const [chartData,setChartData]= useState(null)
+  const [chartData,setChartData]= useState(null);
+  //format the data 
+  const formatData = data=>{
+    return data.map((item)=>{
+     return {
+        t:item[0],
+        y:item[1].toFixed(4)
+      }
+    })
+  }
+
+  
+
+
   useEffect(() => {
-    fetch(`https://api.coingecko.com/api/v3/coins/${idToFetch}/market_chart?vs_currency=gbp&days=2
+    fetch(`https://api.coingecko.com/api/v3/coins/${idToFetch}/market_chart?vs_currency=gbp&days=1&interval=hourly
     `)
       .then((res) => res.json())
       .then((data) => {
-        setChartData(data.prices);
+        setChartData(formatData(data.prices));
       })
       .catch((err) => console.log(err));
   }, [idToFetch]);
   const data = {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    // labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
     datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
+      label: idToFetch,
+      data: [chartData],
       backgroundColor: 'rgba(0, 0, 0, 0.1)',
       borderColor: 'rgba(255, 99, 132, 1)',
-    
+      
       
       borderWidth: 2,
     }],
-  };
+  }
+  const options ={   options: {
+      
+      
+    lineHeightAnnotation: {
+      always:true,
+      hover:false,
+      lineweight:1.5
+    },
+    animation:{
+      duration:2000
+    },
+    maintainApectRatio:true,
+    responsive:true,
+   scales: {
+       xAxes: [{
+           type: 'time',
+           distribution: 'linear'
+       }]
+   }
+}}
+
+
+  
   return (
     <>
     <Line
      data={data}
-     width={100}
-     height={50}
-     options={{ maintainAspectRatio: true }}
+     width={250}
+     height={250}
+     options={options.options}
     />
     <div>
       This is the trending chart
